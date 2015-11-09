652dac951de82fc2d6d5a0b180e4d776f75d9cee
ivmartel <ivmartel@gmail.com>
2015-11-09 01:22:41 +0100

    Creating the dwv war file.

--- resources/update-gh-pages.sh
+++ resources/update-gh-pages.sh
@@ -3,6 +3,13 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
+  #a bit out of scope, while we have a java environment...
+  export DWV_VERSION=0.12.0
+  wget https://github.com/ivmartel/dwv/releases/download/v$DWV_VERSION/dwv-$DWV_VERSION.zip
+  unzip dwv-$DWV_VERSION.zip
+  cd dwv-$DWV_VERSION
+  jar -cvf dwv.war *
+  cd ..
   #setup git
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
@@ -12,6 +19,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   cd gh-pages
   #copy new dist
   cp -R ../dwv-dcm4chee-web/target/*.jar distrib
+  cp -R ../dwv-$DWV_VERSION/*.war distrib
   #add, commit and push files
   git add -Af .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
