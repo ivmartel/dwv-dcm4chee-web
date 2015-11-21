435c406105472ddf927313961508050de5c133a2
ivmartel <ivmartel@gmail.com>
2015-11-21 23:35:42 +0100

    Updated script to only create war once.

--- resources/update-gh-pages.sh
+++ resources/update-gh-pages.sh
@@ -3,23 +3,23 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
-  #a bit out of scope, while we have a java environment...
-  export DWV_VERSION=0.12.0
-  wget https://github.com/ivmartel/dwv/releases/download/v$DWV_VERSION/dwv-$DWV_VERSION.zip
-  unzip dwv-$DWV_VERSION.zip
-  cd dwv-$DWV_VERSION
-  jar -cvf dwv-$DWV_VERSION.war *
-  cd ..
   #setup git
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   #using token clone gh-pages branch
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv-dcm4chee-web.git gh-pages > /dev/null
-  #go into directory and copy data we're interested in to that directory
-  cd gh-pages
+  #a bit out of scope, while we have a java environment...
+  export DWV_VERSION=0.12.0
+  if [ ! -f gh-pages/distrib/dwv-$DWV_VERSION.war ]; then
+    wget https://github.com/ivmartel/dwv/releases/download/v$DWV_VERSION/dwv-$DWV_VERSION.zip
+    unzip dwv-$DWV_VERSION.zip
+    cd dwv-$DWV_VERSION
+    jar -cvf dwv-$DWV_VERSION.war *
+    cp dwv-$DWV_VERSION.war ../gh-pages/distrib
+    cd ..
+  fi
   #copy new dist
-  cp -R ../dwv-dcm4chee-web/target/*.jar distrib
-  cp -R ../dwv-$DWV_VERSION/*.war distrib
+  cp -R dwv-dcm4chee-web/target/*.jar gh-pages/distrib
   #add, commit and push files
   git add -Af .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
